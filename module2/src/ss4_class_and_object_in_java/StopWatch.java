c6447b0a50b5dfc407f10e2c4c7af9e92bf19e6d
Võ Nguyễn Phát <vonguyenphat012@gmail.com>
2022-07-18 18:28:17 +0700

    up

---
+++ module2/src/ss4_class_and_object_in_java/StopWatch.java
@@ -0,0 +1,45 @@
+package ss4_class_and_object_in_java;
+
+import javafx.scene.paint.Stop;
+
+public class StopWatch {
+    private long starTime, endTime;
+
+    public long getStarTime() {
+        return starTime;
+    }
+
+    public void setStarTime(long starTime) {
+        this.starTime = starTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+    public StopWatch(){
+        starTime = System.currentTimeMillis();
+    }
+    public void  start(){
+        starTime = System.currentTimeMillis();
+    }
+    public void end(){
+        endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime(){
+        return endTime-starTime;
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        long x = 0 ;
+        for (long i = 0; i < 100000000 ; i++) {
+        }
+        stopWatch.end();
+        System.out.println(stopWatch.getElapsedTime());
+    }
+}
