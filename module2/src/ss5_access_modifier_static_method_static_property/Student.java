a40ddad111c741536287410675d8b5a29111c085
Võ Nguyễn Phát <vonguyenphat012@gmail.com>
2022-07-20 19:14:39 +0700

    ss5

---
+++ module2/src/ss5_access_modifier_static_method_static_property/Student.java
@@ -0,0 +1,30 @@
+package ss5_access_modifier_static_method_static_property;
+
+public class Student {
+    public String name = "John";
+    public String classes = "CO2";
+
+    public Student() {
+
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    void display() {
+        System.out.printf("name = %s :class = %s%n", getName(), getClasses());
+    }
+}
